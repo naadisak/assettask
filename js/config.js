73c926fe1b4ca763f909d373eec5894cb78caeb6
naadisak <na.adisak@gmail.com>
2026-05-14 12:18:41 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-  GAS_URL:      'https://script.google.com/macros/s/AKfycbztzcbmC5z18LhYchAGtFgaHr-6feOps58nI0IACUVq9LzJhFEF9DGQlbjF1nfqUJMvAQ/exec',
-  IMGBB_KEY:    'b37889052f6fd7b7143ff017d07914df',
-  IMGBB_URL:    'https://api.imgbb.com/1/upload',
-  SESSION_KEY:  'assetsys_7769',
-  NOTE_EMOJIS:  ['✅', '⚠️', '🔧', '💻', '🚫'],
-  APP_NAME:     'AssetSys',
+  GAS_URL:         'https://script.google.com/macros/s/AKfycbztzcbmC5z18LhYchAGtFgaHr-6feOps58nI0IACUVq9LzJhFEF9DGQlbjF1nfqUJMvAQ/exec',
+  IMGBB_KEY:       'b37889052f6fd7b7143ff017d07914df',
+  IMGBB_URL:       'https://api.imgbb.com/1/upload',
+  SESSION_KEY:     'assetsys_7769',
+  NOTE_EMOJIS:     ['✅', '⚠️', '🔧', '💻', '🚫'],
+  APP_NAME:        'AssetSys',
+  LINE_CHANNEL_ID: '',   // ← ใส่ LINE Channel ID เมื่อพร้อม
 };
